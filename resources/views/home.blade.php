61a6a57df197e0053003242895431db5bc82858d
Zineb <zineb@gmail.com>
2026-04-27 08:40:17 +0200

    fix home page + images + cleanup project

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,65 @@
 @extends('layouts.master')
 
 @section('content')
+    <h2 class="mb-4 text-center">✈️ Nos Voyages</h2>
+    
+    <h3 class="mb-3">🌍 Voyages Internationaux</h3>
+    <div class="row">
 
+        @foreach ($voyages->where('type_voyage_id', 2) as $voyage)
+            <div class="col-md-4 mb-3">
+                <div class="card shadow">
+                    <img src="/images/{{ $voyage->image }}" class="card-img-top" style="height:200px; object-fit:cover;">
+                    <div class="card-body text-center" >
+                        <h5>{{ $voyage->destination }}</h5>
+                        <p>{{ $voyage->prix }} DH</p>
+
+                    </div>
+                </div>
+
+            </div>
+        @endforeach
+
+    </div>
+
+
+    <h3 class="mb-3">🌍 Voyages Nationaux</h3>
+    <div class="row">
+
+        @foreach ($voyages->where('type_voyage_id', 1) as $voyage)
+            <div class="col-md-4 mb-3">
+                <div class="card shadow">
+                    <img src="/images/{{ $voyage->image }}" class="card-img-top" style="height:200px; object-fit:cover;">
+                    <div class="card-body text-center" >
+                        <h5>{{ $voyage->destination }}</h5>
+                        <p>{{ $voyage->prix }} DH</p>
+
+                    </div>
+                </div>
+
+            </div>
+        @endforeach
+
+    </div>
+
+
+
+    <h3 class="mb-3">🕋 Omra</h3>
+    <div class="row">
+
+        @foreach ($voyages->where('type_voyage_id', 3) as $voyage)
+            <div class="col-md-4 mb-3">
+                <div class="card shadow">
+                    <img src="/images/{{ $voyage->image }}" class="card-img-top" style="height:200px; object-fit:cover;">
+                    <div class="card-body text-center" >
+                        <h5>{{ $voyage->destination }}</h5>
+                        <p>{{ $voyage->prix }} DH</p>
+
+                    </div>
+                </div>
+
+            </div>
+        @endforeach
+
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,25 @@
 <?php
 
 use App\Livewire\TestPage;
+use App\Models\Voyage;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-
-    return view('home');
+    $voyages = Voyage::all();
+    return view('home',compact('voyages'));
 });
 
 
 
-Route::get('/test', TestPage::class);
+Route::get('/test', function () {
+    return view('components.test');
+});
 
 Route::get('/voyages', function () {
     return view('components.voyages');
 });
+
 
 
 
